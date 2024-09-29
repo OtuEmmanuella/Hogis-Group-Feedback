f8e0b5a4a209f4ff0a7d8b6404dd7e9a57a5dab6
Otu Emmanuella <otumanuella@gmail.com>
2024-09-29 01:27:02 +0100

    fixed root issues

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  "apiKey": "AIzaSyAfPy70gxu_8hrov10Z6Ant_Azm8drxet4",
+  "authDomain": "hogis-group-feedback.firebaseapp.com",
+  "projectId": "hogis-group-feedback",
+  "storageBucket": "hogis-group-feedback.appspot.com",
+  "messagingSenderId": "726346498168",
+  "appId": "1:726346498168:web:22afbdab956fc8ba71ad8d",
+  "measurementId": "G-9RFQDYGB7F"
+};
